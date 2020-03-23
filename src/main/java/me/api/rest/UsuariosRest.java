e015ab6ae0b3b45ab9faecc1d2aa1d3dab6e130d
felipe <walter.rosado2498@gmail.com>
2020-03-22 20:47:45 -0500

    [ADD] @CrossOrigin(*) agregado en el RestController para solucionar el problema al ser consumido por jquery

--- src/main/java/me/api/rest/UsuariosRest.java
+++ src/main/java/me/api/rest/UsuariosRest.java
@@ -3,6 +3,7 @@ package me.api.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import me.api.dao.UsuariosDAO;
 import me.api.model.Usuarios;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/usuarios")//ruta uri
 public class UsuariosRest {
